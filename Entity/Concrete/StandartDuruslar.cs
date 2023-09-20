8d892b6a29a2ecdceed609ba79881a87f77eef93
Mesut Ruzgar <mesutrzgarr@gmail.com>
2023-09-20 21:47:39 +0300

    entityler olusturuldu

---
+++ Entity/Concrete/StandartDuruslar.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class StandartDuruslar : IEntity
+    {
+        public DateTime Baslangic { get; set; }
+        public DateTime Bitis { get; set; }      
+        public string? DurusNedeni { get; set; }
+    }
+}
---
+++ Entity/Concrete/StatuEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public enum StatuEnum
+    {
+        Uretim = 1,
+        Durus = 2
+    }
+}
